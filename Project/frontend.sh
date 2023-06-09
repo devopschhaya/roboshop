db4b567041174ad4b608a55b87d379ff925a9c9a
chhayab54 <chhaya.10@gmail.com>
2023-06-09 11:37:02 +0530

    Add code

--- Project/frontend.sh
+++ Project/frontend.sh
@@ -8,4 +8,4 @@ echo -e "\e[32;34m this is Frontend $a\e[0m"
 echo  this is to test the bash 
 echo  "my project is good"
 echo -e "/t work is good "
-echo -e "Today's date is \e[32;34m $Date \e[0m"
+echo -e "Today's date is \e[36;33m $Date \e[0m"

d1b886353d57e3ec6f36989545070ca3cbab6785
chhayab54 <chhaya.10@gmail.com>
2023-06-09 11:35:33 +0530

    Add code

--- Project/frontend.sh
+++ Project/frontend.sh
@@ -3,7 +3,9 @@
 #we will print a line for frontend folde
 
 a=10
+Date=$(date +%F)
 echo -e "\e[32;34m this is Frontend $a\e[0m"
 echo  this is to test the bash 
 echo  "my project is good"
 echo -e "/t work is good "
+echo -e "Today's date is \e[32;34m $Date \e[0m"

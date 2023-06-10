653964285e93fc1df8b615be2d1b558a00ce0645
chhayab54 <chhaya.10@gmail.com>
2023-06-10 21:12:59 +0530

    add code

--- component/frontend.sh
+++ component/frontend.sh
@@ -2,11 +2,12 @@
 echo "I am a frontend"
 COMPONENT=frontend
 ID=$(id -u)
-if [$ID -ne to 0];then
+if [ $ID -ne to 0 ] ; then
 echo -e "\e[31m this script is expected to be run by a root user \e[0m"
 exit 1
+
 fi
 
-echo -n "Installing Nginx :"
-yum install nginx -y  &>> "/tmp/${COMPONENT}.LOG"
+##echo -n "Installing Nginx :"
+##yum install nginx -y  &>> "/tmp/${COMPONENT}.LOG"
 

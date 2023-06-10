f93b78a49f3a45d88f76d6b287d0ac9eed5397e4
chhayab54 <chhaya.10@gmail.com>
2023-06-10 21:20:49 +0530

    add code

--- component/frontend.sh
+++ component/frontend.sh
@@ -4,10 +4,13 @@ COMPONENT=frontend
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
 echo -e "\e[31m this script is expected to be run by a root user \e[0m"
-
+EXIT 1
 
 fi
 
-##echo -n "Installing Nginx :"
-##yum install nginx -y  &>> "/tmp/${COMPONENT}.LOG"
-
+echo -n "Installing Nginx :"
+yum install nginx -y  &>> "/tmp/${COMPONENT}.LOG"
+if [$? -ne 0]; then
+echo -e "\e[32m install has error or failed \e[0m"
+else
+echo -e "\e[31m Install Succeesful \e[0m"

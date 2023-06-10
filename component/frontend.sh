c42c5a2c3792045a4889868b67642fd342ca9e62
chhayab54 <chhaya.10@gmail.com>
2023-06-10 20:15:31 +0530

    add code

--- component/frontend.sh
+++ component/frontend.sh
@@ -3,7 +3,7 @@ echo "I am a frontend"
 COMPONENT=frontend
 ID=$(id -u)
 if [$ID -ne to 0];then
-echo -e "/e[31m this script is expected to be run by a root user \e[0m"
+echo -e "\e[31m this script is expected to be run by a root user \e[0m"
 exit 1
 fi
 

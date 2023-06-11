b804c96acd65ab283f5188428985b8915f31741a
chhayab54 <chhaya.10@gmail.com>
2023-06-11 08:06:39 +0530

    add code

--- component/frontend.sh
+++ component/frontend.sh
@@ -5,8 +5,16 @@ ID=$(id -u)
 if [ $ID -ne 0 ] ; then
 echo -e "\e[31m this script is expected to be run by a root user \e[0m"
 exit 1
-
 fi
+
+stat() {
+if [ $? -ne 0 ] ; then
+echo -e "\e[32m install has error or failed \e[0m"
+else
+echo -e "\e[31m Install Succeesful \e[0m"
+exit 2
+fi
+}
 
 echo -n "Installing Nginx :"
 yum install nginx -y  &>> "/tmp/${COMPONENT}.LOG"
@@ -14,13 +22,14 @@ if [ $? -ne 0 ] ; then
 echo -e "\e[32m install has error or failed \e[0m"
 else
 echo -e "\e[31m Install Succeesful \e[0m"
+exit2
 fi
-exit 2
+
 echo -n "Installing $COMPONENT :"
 curl /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 if [ $? -ne 0 ] ; then
 echo -e "\e[32m install has error or failed \e[0m"
 else
 echo -e "\e[31m Install Succeesful \e[0m"
-fi
 exit 2
+fi

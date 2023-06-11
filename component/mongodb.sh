0eef860acbba984c9aa6e6a804869f6d3d3ea4d5
chhayab54 <chhaya.10@gmail.com>
2023-06-11 14:58:07 +0530

    add code

--- component/mongodb.sh
+++ component/mongodb.sh
@@ -33,3 +33,12 @@ systemctl daemon-reload mongod      &>> $LOGFILE
 systemctl enable mongod      &>> $LOGFILE
 systemctl restart mongod       &>> $LOGFILE
 stat $?
+
+echo -n "Downloading the $COMPONENT schema:"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+stat $?
+
+echo -n "Extracting the $COMPONENT Schema:"
+cd /tmp
+unzip -o mongodb.zip   &>> $LOGFILE 
+stat $? 

ef95fd981673977b3cc8434f45507f75f7739136
chhayab54 <chhaya.10@gmail.com>
2023-06-12 21:34:15 +0530

    add code

--- component/server.sh
+++ component/server.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId'|sed -e 's/"//g')
-SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Value=devops54" jq '.SecurityGroups[].GroupId' \ sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Value=devops54" jq '.SecurityGroups[].GroupId'|sed -e 's/"//g')
 
 echo -n "AMI id is $AMI_ID"
 echo -n "Security group id is $SG_ID"

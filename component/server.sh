5dd44a0b9cef61af54905d16b30b100054e8f5a6
chhayab54 <chhaya.10@gmail.com>
2023-06-12 21:48:33 +0530

    add code

--- component/server.sh
+++ component/server.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+COMPONENT=$1
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId'|sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=devops54" | jq '.SecurityGroups[].GroupId'|sed -e 's/"//g')
 
@@ -10,7 +12,7 @@ echo -e "Launch instane"
 aws ec2 run-instances  \
      --image-id ${AMI_ID}  \ 
      --instance-type t2.micro  \ 
-     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Chhaya}]' | jq .
+     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]' | jq .
     
     
 

aefdb4d9bad9be03b8d361cf9aab91edbdc84178
chhayab54 <chhaya.10@gmail.com>
2023-06-12 19:50:13 +0530

    add code

--- component/server.sh
+++ component/server.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name ,Values=Devops-LabImage-Centos7" | jq '.Images[].ImageId'|sed -e '/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Devops-LabImage-Centos7" | jq '.Images[].ImageId'|sed -e '/"//g')
 
 echo "AMI id is $AMI_ID"
 

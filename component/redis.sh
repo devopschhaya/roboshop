64e124fc10bc343fcd1d2b5a31663b4e2735543e
chhayab54 <chhaya.10@gmail.com>
2023-06-12 15:15:50 +0530

    add code

--- component/redis.sh
+++ component/redis.sh
@@ -1,8 +1,18 @@
 #!/bash/bin
 echo this is radish
 
-COMPONENT = radish
+COMPONENT = redis
 LOGFILE= "/tmp/${COMPONENT}.log"
+
+stat() {
+if [ $1 -ne 0 ] ; then
+echo -e "\e[32m install has error or failed \e[0m"
+else
+echo -e "\e[31m Install Succeesful \e[0m"
+
+fi
+}
+
 
 echo -n "installing redis"
 curl -L https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo -o /etc/yum.repos.d/$COMPONENT.repo

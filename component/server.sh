f11700db2deac08789d80d2b0afad52c2329091d
chhayab54 <chhaya.10@gmail.com>
2023-06-12 21:32:51 +0530

    add code

--- component/server.sh
+++ component/server.sh
@@ -1,6 +1,13 @@
 #/bin/bash
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId'|sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Value=devops54" jq '.SecurityGroups[].GroupId' \ sed -e 's/"//g')
 
-echo "AMI id is $AMI_ID"
+echo -n "AMI id is $AMI_ID"
+echo -n "Security group id is $SG_ID"
+
+echo -e "Launch instane"
+aws ec2 run-instances --image-id ${AMU_ID} --instance-type t2.micro 
+    
+    
 
